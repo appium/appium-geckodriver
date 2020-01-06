e1aff4eb35992b4e059e03f470723c31dee87391
vrunoa <vruno.alassia@gmail.com>
2020-01-05 16:28:22 -0800

    update unit test to make sure geckodriverExecutable is set

--- test/unit/geckodriver-specs.js
+++ test/unit/geckodriver-specs.js
@@ -7,10 +7,15 @@ let should = chai.should();
 describe('Geckodriver simple test', function () {
   let driver;
   before(function () {
-    driver = new Geckodriver();
+    driver = new Geckodriver({
+      geckodriverExecutable: '/path/to/geckodriver',
+    });
   });
 
-  it('should exists', function () {
+  it('should exists and set properties', function () {
     should.exist(driver);
+    driver.geckodriverExecutable.should.equal('/path/to/geckodriver');
+    driver.noReset.should.equal(false);
   });
+
 });
