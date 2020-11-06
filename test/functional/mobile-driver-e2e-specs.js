164d65352bab53eb5c51d7acd7aa0a6eb1ae03d0
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-06 17:22:30 +0100

    feat: Initial driver setup

---
+++ test/functional/mobile-driver-e2e-specs.js
@@ -0,0 +1,54 @@
+import wd from 'wd';
+import { startServer } from '../..';
+import chaiAsPromised from 'chai-as-promised';
+import chai from 'chai';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+const HOST = '127.0.0.1';
+const PORT = 4567;
+const MOCHA_TIMEOUT = 240000;
+const DEVICE_NAME = process.env.DEVICE_NAME || 'emulator-5554';
+// The Firefox binary could be retrieved from https://www.mozilla.org/en-GB/firefox/all/#product-android-release
+const CAPS = {
+  verbosity: 'trace',
+  'moz:firefoxOptions': {
+    androidDeviceSerial: DEVICE_NAME,
+    androidPackage: 'org.mozilla.firefox',
+  },
+};
+
+describe('Mobile SafariDriver', function () {
+  this.timeout(MOCHA_TIMEOUT);
+
+  let server;
+  let driver;
+  before(async function () {
+    server = await startServer(PORT, HOST);
+  });
+  after(async function () {
+    if (server) {
+      await server.close();
+      server = null;
+    }
+  });
+  beforeEach(async function () {
+    driver = wd.promiseChainRemote(HOST, PORT);
+    await driver.init(CAPS);
+  });
+  afterEach(async function () {
+    if (driver) {
+      await driver.quit();
+      driver = null;
+    }
+  });
+
+  it('should start and stop a session', async function () {
+    await driver.get('https://appium.io/');
+    const button = await driver.elementByCss('#downloadLink');
+    await button.text().should.eventually.eql('Download Appium');
+  });
+});
+
+
