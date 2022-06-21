049943234820131ad4d4957963655345d83636dc
Mykola Mokhnach <mokhnach@gmail.com>
2022-06-21 15:21:06 +0200

    Tune start server

--- test/server.js
+++ test/server.js
@@ -1,8 +1,22 @@
-import { main as startAppium } from 'appium';
+import { SubProcess } from 'teen_process';
 
 async function startServer (port, address, relaxedSecurityEnabled = false) {
   // https://github.com/appium/appium/blob/2.0/packages/schema/lib/appium-config.schema.json
-  return await startAppium({port, address, relaxedSecurityEnabled});
+  const args = ['server'];
+  if (port) {
+    args.push('--port', port);
+  }
+  if (address) {
+    args.push('--address', address);
+  }
+  if (relaxedSecurityEnabled) {
+    args.push('--relaxed-security');
+  }
+  const process = new SubProcess('appium', args);
+  await process.start();
+  return {
+    close: async () => { await process.stop(); }
+  };
 }
 
 export { startServer };
