f1c5ee8dc42d79de7e83041f5d22c43f723d6bc9
Mykola Mokhnach <mokhnach@gmail.com>
2024-06-23 15:06:16 +0200

    chore: Bump chai and chai-as-promised

--- test/unit/driver-specs.js
+++ test/unit/driver-specs.js
@@ -1,8 +1,14 @@
 import GeckoDriver from '../../lib/driver';
-import chai from 'chai';
-const should = chai.should();
 
 describe('GeckoDriver', function () {
+  let chai;
+  let should;
+
+  before(async function () {
+    chai = await import('chai');
+    should = chai.should();
+  });
+
   it('should exist', function () {
     should.exist(GeckoDriver);
   });
