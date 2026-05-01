7801ff1d502c0678f328b81c3f8d753c125aa6ec
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-04-30 23:33:03 -0700

    fix typo

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,7 +26,7 @@ export function formatCapsForServer(caps: StringRecord): StringRecord {
   }
   if (result.platformName) {
     // Geckodriver only supports lowercase platform names
-    result.platformName = `${result.platformName}`.toLowerCase();
+    result.platformName = result.platformName.toLowerCase();
   }
   return result;
 }
@@ -49,7 +49,7 @@ export async function mkdirp(p: string): Promise<void> {
  * Extract a specific file from a tar.gz archive
  */
 export async function extractFileFromTarGz(
-  srcAcrhive: string,
+  srcArchive: string,
   fileToExtract: string,
   dstPath: string,
 ): Promise<void> {
@@ -78,7 +78,7 @@ export async function extractFileFromTarGz(
       } else {
         return reject(
           new Error(
-            `The file '${fileToExtract}' could not be found in the '${srcAcrhive}' archive`,
+            `The file '${fileToExtract}' could not be found in the '${srcArchive}' archive`,
           ),
         );
       }
@@ -86,7 +86,7 @@ export async function extractFileFromTarGz(
     });
   });
 
-  fs.createReadStream(srcAcrhive).pipe(zlib.createGunzip()).pipe(extract);
+  fs.createReadStream(srcArchive).pipe(zlib.createGunzip()).pipe(extract);
 
   await extractPromise;
 }
@@ -95,12 +95,12 @@ export async function extractFileFromTarGz(
  * Extract a specific file from a zip archive
  */
 export async function extractFileFromZip(
-  srcAcrhive: string,
+  srcArchive: string,
   fileToExtract: string,
   dstPath: string,
 ): Promise<void> {
   let didFindEntry = false;
-  await zip.readEntries(srcAcrhive, async ({entry, extractEntryTo}) => {
+  await zip.readEntries(srcArchive, async ({entry, extractEntryTo}) => {
     if (didFindEntry || entry.fileName !== fileToExtract) {
       return;
     }
@@ -116,7 +116,7 @@ export async function extractFileFromZip(
   });
   if (!didFindEntry) {
     throw new Error(
-      `The file '${fileToExtract}' could not be found in the '${srcAcrhive}' archive`,
+      `The file '${fileToExtract}' could not be found in the '${srcArchive}' archive`,
     );
   }
 }
