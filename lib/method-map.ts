7811050feb14c35f5aa2dfc6b9ee8e759bc63fb8
Edgars Eglītis <37242620+eglitise@users.noreply.github.com>
2026-06-10 14:03:34 +0300

    feat: add 5 Firefox WebDriver endpoints (#167)
    * feat: add 5 Firefox WebDriver endpoints
    
    * feat: support 'path' parameter for installAddon

---
+++ lib/method-map.ts
@@ -0,0 +1,39 @@
+import type {MethodMap} from '@appium/types';
+import type {GeckoDriver} from './driver';
+
+export const newMethodMap = {
+  /**
+   * Firefox vendor-specific endpoints
+   * https://github.com/mozilla-firefox/firefox/blob/main/testing/geckodriver/src/command.rs
+   */
+  '/session/:sessionId/moz/context': {
+    GET: {
+      command: 'getContext',
+    },
+    POST: {
+      command: 'setContext',
+      payloadParams: {required: ['context']},
+    },
+  },
+  '/session/:sessionId/moz/addon/install': {
+    POST: {
+      command: 'installAddon',
+      payloadParams: {
+        validate: (payloadData) =>
+          !payloadData.addon && !payloadData.path && "Either 'addon' or 'path' must be provided",
+        optional: ['addon', 'path', 'temporary', 'allowPrivateBrowsing'],
+      },
+    },
+  },
+  '/session/:sessionId/moz/addon/uninstall': {
+    POST: {
+      command: 'uninstallAddon',
+      payloadParams: {required: ['id']},
+    },
+  },
+  '/session/:sessionId/moz/screenshot/full': {
+    GET: {
+      command: 'takeFullScreenshot',
+    },
+  },
+} as const satisfies MethodMap<GeckoDriver>;
